9ab9f7d1226124c1b04f9c9bb3cfc416f6593c0d
Vaibhavtiwari079 <vaibhavtiwari.0504@gmail.com>
2024-07-31 18:29:40 +0530

    after learning nest

---
+++ apps/api/src/models/users/graphql/dtos/update-user.input.ts
@@ -0,0 +1,9 @@
+
+import { InputType, PartialType } from '@nestjs/graphql'
+
+import { User } from '../entity/user.entity'
+
+@InputType()
+export class UpdateUserInput extends PartialType(User) {
+  uid: User['uid']
+}
